08ec4933aafd5422e5c0715805125b401927fa93
Pedro Santos <donbonifacio@gmail.com>
2016-06-30 10:48:46 +0100

    Improve json handling
    Start by properly catching a json exception and notify and also show the
    body that generated the error.
    
    This was needed because we were having an error with a hidden char
    (65279) that we did not see but was there. We added something to clear
    that.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weareswat/request-utils "0.4.0"
+(defproject weareswat/request-utils "0.4.1"
   :description "Some utilities to do async http requests with aleph"
   :url "https://github.com/weareswat/request-utils"
   :license {:name         "MIT"
